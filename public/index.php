da286afe327257ac026035054f6b9db1ccd6dc17
Pimpys <maxkyivua@gmail.com>
2020-04-21 19:51:43 +0300

    add class

--- public/index.php
+++ public/index.php
@@ -1,3 +1,24 @@
+<pre>
 <?php
 
-phpinfo();
+class Car
+{
+    protected $wheels;
+
+    public $doors;
+
+    private $engine;
+
+    public function setMotor($engine)
+    {
+        $this->engine = $engine;
+    }
+}
+
+class BMW extends Car
+{
+}
+
+$car = new BMW();
+$car->setMotor('motor');
+var_dump($car);
